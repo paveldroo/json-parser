e113feae88aaeb6ad54b4fa014e2e2e4ba9eea9e
Pavel Druzhinin <paveldroo@gmail.com>
2026-07-06 23:57:22 +0300

    add step2 green tests

--- src/parser.rs
+++ src/parser.rs
@@ -5,25 +5,42 @@ pub fn parse_json(input: &str) -> Result<(), Box<dyn Error>> {
         return Err("json invalid".into());
     }
 
-    let pairs = HashMap::from([('}', '{')]);
+    let pairs = HashMap::from([('}', '{'), ('{', '}'), ('"', '"')]);
     let mut parentheses: Vec<char> = Vec::new();
 
-    for ch in input.chars() {
-        if parentheses.is_empty() {
+    for (idx, ch) in input.chars().enumerate() {
+        if parentheses.is_empty() && pairs.contains_key(&ch) {
+            println!("pushing char {ch}");
             parentheses.push(ch);
             continue;
         }
 
-        if parentheses.last() == pairs.get(&ch) {
-            parentheses.pop();
-        } else {
-            parentheses.push(ch);
+        if ch == '}' && idx > 0 && input.chars().nth(idx - 1).unwrap() == ',' {
+            return Err("json invalid".into());
+        }
+
+        if ch == ':' && idx > 0 && input.chars().nth(idx - 1).unwrap() != '"' {
+            return Err("json invalid".into());
+        }
+
+        match pairs.get(&ch) {
+            Some(c) => {
+                if parentheses.last().unwrap() == c {
+                    parentheses.pop();
+                } else {
+                    parentheses.push(*c);
+                }
+            }
+            _ => continue,
         }
     }
 
     match parentheses.len() {
         0 => Ok(()),
-        _ => Err("json invalid".into()),
+        _ => {
+            dbg!(parentheses);
+            Err("json invalid".into())
+        }
     }
 }
 
@@ -32,20 +49,38 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_step1_valid() {
-        let data = crate::reader::read_file("tests/fixtures/step1/valid.json").unwrap();
+    fn test_step2_valid() {
+        let data = crate::reader::read_file("tests/fixtures/step2/valid.json").unwrap();
         match parse_json(&data) {
-            Ok(_) => assert!(true),
-            Err(_) => assert!(false),
+            Ok(_) => assert!(true, "json data: {data:?}"),
+            Err(_) => assert!(false, "json data: {data:?}"),
         }
     }
 
     #[test]
-    fn test_step1_invalid() {
-        let data = crate::reader::read_file("tests/fixtures/step1/invalid.json").unwrap();
+    fn test_step2_invalid() {
+        let data = crate::reader::read_file("tests/fixtures/step2/invalid.json").unwrap();
         match parse_json(&data) {
-            Ok(_) => assert!(false),
-            Err(_) => assert!(true),
+            Ok(_) => assert!(false, "json data: {data:?}"),
+            Err(_) => assert!(true, "json data: {data:?}"),
+        }
+    }
+
+    #[test]
+    fn test_step2_valid2() {
+        let data = crate::reader::read_file("tests/fixtures/step2/valid2.json").unwrap();
+        match parse_json(&data) {
+            Ok(_) => assert!(true, "json data: {data:?}"),
+            Err(_) => assert!(false, "json data: {data:?}"),
+        }
+    }
+
+    #[test]
+    fn test_step2_invalid2() {
+        let data = crate::reader::read_file("tests/fixtures/step2/invalid2.json").unwrap();
+        match parse_json(&data) {
+            Ok(_) => assert!(false, "json data: {data:?}"),
+            Err(_) => assert!(true, "json data: {data:?}"),
         }
     }
 }
