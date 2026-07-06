b4ffff9a76f4f94fab002b728433550d54fd23e2
Pavel Druzhinin <paveldroo@gmail.com>
2026-07-06 12:22:11 +0300

    main boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        eprintln!("json-parser: {err}");
+        std::process::exit(1)
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    Ok(())
 }
