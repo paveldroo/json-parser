286aaa81e3728221cc0fe4c03d53a2258abe36f1
Pavel Druzhinin <paveldroo@gmail.com>
2026-07-08 00:09:10 +0300

    add parsing for bool, null and numeric

--- src/parser.rs
+++ src/parser.rs
@@ -36,6 +36,15 @@ pub fn parse_json(mut input: String) -> Result<(), Box<dyn Error>> {
 fn parse_pair(input: &str) -> Result<(), Box<dyn Error>> {
     let splits: Vec<&str> = input.split(":").map(|token| token.trim()).collect();
     for t in splits.iter() {
+        if !t.starts_with('"') && !t.ends_with('"') {
+            if *t == "true" || *t == "false" || *t == "null" {
+                continue;
+            }
+            let numeric = match t.parse::<i32>() {
+                Ok(_) => continue,
+                Err(_) => return Err("json invalid".into()),
+            };
+        }
         if !t.starts_with('"') || !t.ends_with('"') {
             return Err("json invalid".into());
         }
@@ -95,6 +104,24 @@ mod tests {
     #[test]
     fn test_step2_invalid2() {
         let data = crate::reader::read_file("tests/fixtures/step2/invalid2.json").unwrap();
+        match parse_json(data.to_string()) {
+            Ok(_) => assert!(false, "json data: {data:?}"),
+            Err(_) => assert!(true, "json data: {data:?}"),
+        }
+    }
+
+    #[test]
+    fn test_step3_valid() {
+        let data = crate::reader::read_file("tests/fixtures/step3/valid.json").unwrap();
+        match parse_json(data.to_string()) {
+            Ok(_) => assert!(true, "json data: {data:?}"),
+            Err(_) => assert!(false, "json data: {data:?}"),
+        }
+    }
+
+    #[test]
+    fn test_step3_invalid() {
+        let data = crate::reader::read_file("tests/fixtures/step3/invalid.json").unwrap();
         match parse_json(data.to_string()) {
             Ok(_) => assert!(false, "json data: {data:?}"),
             Err(_) => assert!(true, "json data: {data:?}"),
